68d9053f8eb90a24f44709f52a10bddb8f422e27
Elyormuhandis <elyormuhandis@gmail.com>
2022-10-29 13:23:04 +0500

    sass added

--- src/pages/login/login.component.jsx
+++ src/pages/login/login.component.jsx
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import { userLogin } from '../../features/user/userActions'
 import { useEffect } from 'react'
 import Error from '../../components/Error'
+import './login.styles.scss'
 
 const Login = () => {
   const { loading, userInfo, error } = useSelector((state) => state.user)
@@ -29,34 +30,36 @@ const Login = () => {
   }
 
   return (
-    <div className='login-container'>
-        <form onSubmit={handleSubmit(submitForm)}>
-      {error && <Error>{error}</Error>}
-          <input
-          {...register('username', {
-            required:"To'ldirilishi shart!",
-            minLength:{
-              value: 5,
-              message:"Belgilar soni 5 tadan kam!"
-            }
-          })}
-          required
-        />
-          {errors?.username && <p>{errors?.username?.message || "Error!"}</p>}
-        <input
-          type='password'
-          {...register('password',{
-              required:"To'ldirilishi shart!",
-              minLength:{
+    <div className='login'>
+        <div className='login__container'>
+            <form onSubmit={handleSubmit(submitForm)}>
+               {error && <Error>{error}</Error>}
+            <input
+            {...register('username', {
+                required:"To'ldirilishi shart!",
+                minLength:{
                 value: 5,
                 message:"Belgilar soni 5 tadan kam!"
-              }
-          })}
-          required
-        />
-        {errors?.password && <p>{errors?.password?.message || "Error!"}</p>}
-        <button type='submit' disabled={!isValid}>KIRISH</button>
-    </form>
+                }
+            })}
+            required
+            />
+            {errors?.username && <p>{errors?.username?.message || "Error!"}</p>}
+            <input
+            type='password'
+            {...register('password',{
+                required:"To'ldirilishi shart!",
+                minLength:{
+                    value: 5,
+                    message:"Belgilar soni 5 tadan kam!"
+                }
+            })}
+            required
+            />
+            {errors?.password && <p>{errors?.password?.message || "Error!"}</p>}
+            <button type='submit' disabled={!isValid}>KIRISH</button>
+            </form>
+        </div>
     </div>
   )
 }
